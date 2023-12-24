96eff8422d44048f2b246967c60ae98fd13fecf7
Sanjay7773 <149701770+Sanjay7773@users.noreply.github.com>
2023-12-24 16:51:38 +0530

    ak button ke click par color badala
    click event ok

---
+++ main.js
@@ -0,0 +1,7 @@
+var bt1 = document.querySelector("#bt1");
+var h1 = document.querySelector("#h1");
+
+bt1.addEventListener("click", function(){
+    h1.style.color ="blue"
+// alert("chala means ==> OK  ")
+})
