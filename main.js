74d4b072316ce9d9aaaafa510fdb6a60d10d3ea0
Sanjay7773 <149701770+Sanjay7773@users.noreply.github.com>
2023-12-27 14:23:26 +0530

    swap img ok

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+//[1]=>   click a 1button and change some text what you want change like EVENT HANDLE
 var bt1 = document.querySelector("#bt1");
 var h1 = document.querySelector("#h1");
 
-bt1.addEventListener("click", function(){
-    h1.style.color ="blue"
-// alert("chala means ==> OK  ")
+bt1.addEventListener("mouseover", function () {
+    h1.style.color = "blue"
+    bt1.style.color = "green"
+    // alert("chala means ==> OK  ")
 })
+bt1.addEventListener("mouseout", function () {
+    h1.style.color = "black"
+    bt1.style.color = "black"
+
+    // alert("chala means ==> OK  ")
+})
+
+
+//[2]=> click a button and two img exchage to our position swap-place togather
+
+var img1 = document.querySelector("#img1");
+var img2 = document.querySelector("#img2");
+var swap = document.querySelector("#swap");
+swap.addEventListener("click", function () {
+    // alert("chala  ok");
+       var a = img1.src ;
+       var b = img2.src;
+       img1.src = b;
+       img2.src = a;
+});
+
+
+//[3]=> 
+
